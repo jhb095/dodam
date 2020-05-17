2f198de859f18832fac8cb88bde69b6e1c6c8cd0
orang <iseungsoo07@gmail.com>
2020-05-18 01:52:37 +0900

    SignUp layout

--- app/src/main/java/com/example/dodam/MainActivity.java
+++ app/src/main/java/com/example/dodam/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.dodam;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView sign_up;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_main);
+
+        sign_up = findViewById(R.id.sign_up);
+        sign_up.setClickable(true);
+
+        sign_up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/dodam/SignUpActivity.java
@@ -0,0 +1,17 @@
+package com.example.dodam;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+
+    }
+}
