2854b2bfaca59f681458882d8a4792d540ba6716
iseungsoo07 <iseungsoo07@gmail.com>
2020-05-25 19:54:42 +0900

    add activity_home.xml

--- app/src/main/java/com/example/dodam/MainActivity.java
+++ app/src/main/java/com/example/dodam/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     private TextView sign_up;
+    private TextView sign_in;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,12 +18,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         sign_up = findViewById(R.id.sign_up);
+        sign_in = findViewById(R.id.sign_in);
         sign_up.setClickable(true);
+        sign_in.setClickable(true);
 
         sign_up.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        sign_in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SignInActivity.class);
                 startActivity(intent);
             }
         });
