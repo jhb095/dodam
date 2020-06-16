80ec7997e5e710fa7d886da9993bfece93de866d
iseungsoo07 <iseungsoo07@gmail.com>
2020-06-16 21:45:56 +0900

    브랜드 랭킹 레이아웃 추가

---
+++ app/src/main/java/com/example/dodam/home/BrandRanking.java
@@ -0,0 +1,16 @@
+package com.example.dodam.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.dodam.R;
+
+public class BrandRanking extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.brand_ranking);
+    }
+}
